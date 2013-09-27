a880dc37ffc5928cddaccf07f3a42c05e6d54c44
Dan Mills <code@sandmill.org>
2013-09-27 13:51:03 -0700

    cleaner, more responsive design

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,5 +3,11 @@
 angular.module('foobarApp')
   .controller('MainCtrl', function ($scope) {
     $scope.format = 'HTML';
+    $scope.license = {
+      deedUrl: 'https://creativecommons.org/licenses/by/3.0/',
+      lang: 'en_US',
+      iconUrl: 'http://i.creativecommons.org/l/by/3.0/80x15.png',
+      title: 'Creative Commons Attribution 3.0 Unported'
+    };
   });
 
